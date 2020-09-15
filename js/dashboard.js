624ec16b152641efeeaff09dfebfdaa8250ba098
ngangavic <ngangavictor10@gmail.com>
2020-09-15 08:34:26 +0300

    show on sidebar click

--- js/dashboard.js
+++ js/dashboard.js
@@ -13,3 +13,43 @@ $("#project").css('display','none')
 $("#skills").css('display','none')
 $("#testimonials").css('display','none')
 }
+
+function showDetails() {
+    $("#details").css('display','inline')
+    $("#education").css('display','none')
+    $("#project").css('display','none')
+    $("#skills").css('display','none')
+    $("#testimonials").css('display','none')
+}
+
+function showEducation() {
+    $("#details").css('display','none')
+    $("#education").css('display','inline')
+    $("#project").css('display','none')
+    $("#skills").css('display','none')
+    $("#testimonials").css('display','none')
+}
+
+function showProjects() {
+    $("#details").css('display','none')
+    $("#education").css('display','none')
+    $("#project").css('display','inline')
+    $("#skills").css('display','none')
+    $("#testimonials").css('display','none')
+}
+
+function showSkills() {
+    $("#details").css('display','none')
+    $("#education").css('display','none')
+    $("#project").css('display','none')
+    $("#skills").css('display','inline')
+    $("#testimonials").css('display','none')
+}
+
+function showTestimonials() {
+    $("#details").css('display','none')
+    $("#education").css('display','none')
+    $("#project").css('display','none')
+    $("#skills").css('display','none')
+    $("#testimonials").css('display','inline')
+}
