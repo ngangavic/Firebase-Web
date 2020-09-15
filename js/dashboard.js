3e88470070f01801eb486a561603ba2df6f8efad
ngangavic <ngangavictor10@gmail.com>
2020-09-15 09:25:46 +0300

    save details data

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,3 +1,22 @@
+function saveDetailsData() {
+    const username = document.getElementById("username").value;
+    const email = document.getElementById("email").value;
+    const phone = document.getElementById("phone").value;
+    const job = document.getElementById("job").value;
+    const about = document.getElementById("about-user").value;
+
+    alert("User data:\n"+username+"\n"+email+"\n"+phone+"\n"+job+"\n"+about)
+    firebase.database().ref('/porfolio/' + firebase.auth().currentUser.uid +'/my-details').set({
+        username:username,
+        email:email,
+        phone:phone,
+        job:job,
+        about:about
+    }).then(function (result) {
+alert("Message: "+result)
+    })
+}
+
 function logout() {
     firebase.auth().signOut().then(function () {
         window.location.replace("index.html");
